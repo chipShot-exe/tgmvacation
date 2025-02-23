63cd7a04c65eb18671532b7b8168b9324f4613aa
chipShot-exe <leeyummartin@gmail.com>
2025-02-22 19:09:14 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const textarea = document.getElementById("notes");
+const maxChars = 5242870; // Set your character limit here
+const charCount = document.getElementById("charCount");
+const storageKey = "userNotes";
+
+// Fetch value from localStorage and populate textarea
+window.addEventListener("load", () => {
+    textarea.value = localStorage.getItem(storageKey);
+    updateCharCount();
+});
+
+textarea.addEventListener("input", () => {
+    const currentValue = textarea.value;
+    textarea.value = ""; // Clear the textarea
+    textarea.value = currentValue.substring(0, maxChars); // Re-enter trimmed value
+    localStorage.setItem(storageKey, textarea.value);
+    updateCharCount();
+});
+
+function updateCharCount() {
+    charCount.textContent = `${textarea.value.length}/${maxChars}`;
+}
+
+function changeTitle(value) {
+    document.title = "Disneyland Notepad | " + value;
+}
+const tipsTab = document.getElementById('pills-tips-tab');
+tipsTab.addEventListener('click', function() {
+    changeTitle("Tips");
+});
+
+const notesTab = document.getElementById('pills-notes-tab');
+notesTab.addEventListener('click', function() {
+    changeTitle("Notes");
+});
+
+const shopTab = document.getElementById('pills-shop-tab');
+shopTab.addEventListener('click', function() {
+    changeTitle("Shopping");
+});
+
