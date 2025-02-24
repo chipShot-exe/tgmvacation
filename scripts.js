3ebf6ad28fcf19a5c3a60829ea1ee970755c974c
chipShot-exe <leeyummartin@gmail.com>
2025-02-23 20:01:36 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -37,8 +37,3 @@ const shopTab = document.getElementById('pills-shop-tab');
 shopTab.addEventListener('click', function() {
     changeTitle("Shopping");
 });
-document.addEventListener("DOMContentLoaded", function () {
-   const modal = new bootstrap.Modal(document.getElementById('welcomeModal'));
-   modal.show();
-});
-
