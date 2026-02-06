128317c0002375302b99c263b19c562657ae7e6a
Liam Martin <leeyummartin@gmail.com>
2026-02-06 23:06:24 +0000

    Revert "add showtimes, fix lighting lane, help with mobile and glass styling"
    This reverts commit d0b71c5e9abfaded72cc79f5835e0213541e3713.

--- service-worker.js
+++ service-worker.js
@@ -10,9 +10,9 @@ const PRECACHE_ASSETS = [
   '/pagecommon.js',
   '/DisneylandCropped.jpg',
   '/fuelrod.jpg',
-  '/Unofficial_guide.jpg',
-  '/LLSR.html'
+  '/Unofficial_guide.jpg'
 ];
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
